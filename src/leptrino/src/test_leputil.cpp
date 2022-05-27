7cf5e85213c200dd0b2640ca1d5794826a497e8a
harumo11 <s_harumo@hotmail.co.jp>
2022-05-27 18:15:37 +0900

    fix typo

--- src/leptrino/src/test_leputil.cpp
+++ src/leptrino/src/test_leputil.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[])
     std::cout << "[Info] application starts." << std::endl;
     leptrino sensor;
     while (true) {
-        auto measured_data = sensor.reveive_data();
+        auto measured_data = sensor.receive_data();
         for (auto&& e : measured_data) {
             std::cout << std::fixed << std::setprecision(3) << e << "\t";
         }
